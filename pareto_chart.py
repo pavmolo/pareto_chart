736e1e373a92e6d0e162c0ac70ddba75587a76ff
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-06-02 16:00:49 +0300

    Update pareto_chart.py

--- pareto_chart.py
+++ pareto_chart.py
@@ -10,6 +10,7 @@ from plotly.graph_objects import Figure, Scatter, Bar
 uploaded_file = st.file_uploader("Выберите XLSX файл", accept_multiple_files=False)
 df = pd.read_excel(uploaded_file)
 df_1 = df.set_index(df.columns[0])[df.columns[1]]
+df_1 = df_1(ascending=False)
 st.dataframe(df_1)
 """
 data = pd.read_excel(uploaded_file)
