a3907b0f0e43afd7f918484e0fdc2d490c076477
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-06-02 23:05:34 +0300

    Update pareto_chart.py

--- pareto_chart.py
+++ pareto_chart.py
@@ -38,8 +38,8 @@ if uploaded_file:
       data = pd.concat([data_without_tail, pd.Series(data_tail.sum(), index=data_without_tail.columns, name='Прочее').to_frame().T], axis=0)
       #st.dataframe(data)
     # Выводим график Парето
-    data = [Bar(name = "Объемы",  x= data.index, y= data[i], marker= {"color": list(np.repeat('rgb(71, 71, 135)', 5)) + list(np.repeat('rgb(112, 111, 211)', len(data.index) - 5))}),
-            Scatter(line= {"color": "rgb(192, 57, 43)", "width": 3}, name= "Суммарные проценты", x=  data.index, y= data['cumsum'], yaxis= "y2", mode='lines+markers'),]
+    data = [Bar(name = "Объемы",  x= data.index, y= data[i], marker= {"color": list(np.repeat('rgb(16, 50, 115)', 5)) + list(np.repeat('rgb(0, 163, 194)', len(data.index) - 5))}),
+            Scatter(line= {"color": "rgb(233, 72, 64)", "width": 3}, name= "Суммарные проценты", x=  data.index, y= data['cumsum'], yaxis= "y2", mode='lines+markers'),]
     layout = {"title": {'text': f"Парето {i}", 'font': dict(size=30)}, "font": {"size": 14, "color": "rgb(44, 44, 84)", "family": "Arial"},
               "margin": {"b": 20, "l": 50, "r": 50, "t": 10,}, "height": 800, "width": 1200,
               "plot_bgcolor": "rgb(255, 255, 255)", "legend": {"x": 0.79, "y": 1.2, "font": {"size": 12, "color": "rgb(16, 50, 115)", "family": "Arial"}, 'orientation': 'h',},
