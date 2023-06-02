93788c92f4ed7df9ec75f02bbdf7261634c433ba
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-06-02 16:27:43 +0300

    Update pareto_chart.py

--- pareto_chart.py
+++ pareto_chart.py
@@ -17,7 +17,7 @@ data.columns = [df.columns[0], 'cumsum']
 st.dataframe(data)
 df = data
 data = [Bar(name = "Count",  x= df.index, y= df[df.columns[0]], marker= {"color": list(np.repeat('rgb(71, 71, 135)', 5)) + list(np.repeat('rgb(112, 111, 211)', len(df.index) - 5))}),
-        Scatter(line= {"color": "rgb(192, 57, 43)", "width": 3}, name= "Percentage", x=  df.index, y= df['cumulative'], yaxis= "y2", mode='lines+markers'),]
+        Scatter(line= {"color": "rgb(192, 57, 43)", "width": 3}, name= "Percentage", x=  df.index, y= df['cumsum'], yaxis= "y2", mode='lines+markers'),]
 layout = {"title": {'text': f"{col} Pareto", 'font': dict(size=30)}, "font": {"size": 14, "color": "rgb(44, 44, 84)", "family": "Times New Roman, monospace"},
           "margin": {"b": 20, "l": 50, "r": 50, "t": 10,}, "height": 400, 
           "plot_bgcolor": "rgb(255, 255, 255)", "legend": {"x": 0.79, "y": 1.2, "font": {"size": 12, "color": "rgb(44, 44, 84)", "family": "Courier New, monospace"}, 'orientation': 'h',},
