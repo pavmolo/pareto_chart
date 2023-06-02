69ab10a327156140938c03ae7573545595f0bd43
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-06-02 16:25:09 +0300

    Update pareto_chart.py

--- pareto_chart.py
+++ pareto_chart.py
@@ -19,10 +19,10 @@ df = data
 data = [Bar(name = "Count",  x= df.index, y= df.columns[0], marker= {"color": list(np.repeat('rgb(71, 71, 135)', 5)) + list(np.repeat('rgb(112, 111, 211)', len(df.index) - 5))}),
         Scatter(line= {"color": "rgb(192, 57, 43)", "width": 3}, name= "Percentage", x=  df.index, y= df['cumulative'], yaxis= "y2", mode='lines+markers'),]
 layout = {"title": {'text': f"{col} Pareto", 'font': dict(size=30)}, "font": {"size": 14, "color": "rgb(44, 44, 84)", "family": "Times New Roman, monospace"},
-            "margin": {"b": 20, "l": 50, "r": 50, "t": 10,}, "height": 400, 
-            "plot_bgcolor": "rgb(255, 255, 255)", "legend": {"x": 0.79, "y": 1.2, "font": {"size": 12, "color": "rgb(44, 44, 84)", "family": "Courier New, monospace"}, 'orientation': 'h',},
-            "yaxis": {"title": f"Count {col}", "titlefont": {"size": 16, "color": "rgb(71, 71, 135)", "family": "Courier New, monospace"},}, 
-            "yaxis2": {"side": "right", "range": [0, 100], "title": f"Percentage {col}", "titlefont": {"size": 16, "color": "rgb(71, 71, 135)", "family": "Courier New, monospace"}, "overlaying": "y", "ticksuffix": " %",},}
+          "margin": {"b": 20, "l": 50, "r": 50, "t": 10,}, "height": 400, 
+          "plot_bgcolor": "rgb(255, 255, 255)", "legend": {"x": 0.79, "y": 1.2, "font": {"size": 12, "color": "rgb(44, 44, 84)", "family": "Courier New, monospace"}, 'orientation': 'h',},
+          "yaxis": {"title": f"Count {col}", "titlefont": {"size": 16, "color": "rgb(71, 71, 135)", "family": "Courier New, monospace"},}, 
+          "yaxis2": {"side": "right", "range": [0, 100], "title": f"Percentage {col}", "titlefont": {"size": 16, "color": "rgb(71, 71, 135)", "family": "Courier New, monospace"}, "overlaying": "y", "ticksuffix": " %",},}
 fig = Figure(data=data, layout=layout)
 st.plotly_chart(fig)
 
